39d7a99af2716930ffe24c6dfdbdff1f54da76e7
steve <Stevensumaya1@gmail.com>
2025-11-15 14:39:44 -0800

    Created class Movie and MovieLibrary. Movie holds current movie info and set movie info function. Movie Library Holds function printMain which is used as a main menu and function printLibrary without any code inside.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,46 @@
 
 #include <iostream>
 
+using namespace std;
+
+class Movie {
+private:
+    string name;
+    string genre;
+    int year;
+    double rating;
+    bool watchStatus;
+public:
+    void setMovieInfo(string currName, string currGenre, int currYear) {
+        name = currName;
+        genre = currGenre;
+        year = currYear;
+    };
+};
+
+class MovieLibrary {
+private:
+public:
+    void printMain() const{
+        cout << "-------------------------\n";
+        cout << "      Movie Library      \n";
+        cout << "-------------------------\n";
+        cout << "Enter: \n";
+        cout << "1 to print Library \n";
+        cout << "2 to enter a movie \n";
+    };
+    void printLibrary();
+};
+
 int main() {
-    //testttt
+    MovieLibrary Library;
+    Library.printMain();
+    int choice;
+    cin >> choice;
+    
+    if (choice == 1) {
+        Library.printLibrary();
+    }
     return 0;
+
 }
