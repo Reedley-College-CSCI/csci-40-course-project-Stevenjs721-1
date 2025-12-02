e5c639ab8fbca8012c474f5cfada3bf34db49b37
steve <Stevensumaya1@gmail.com>
2025-12-01 19:10:12 -0800

    Updated description.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
  * Description:
  * A movie library that allows users to input, store, and filter a through
  * movies. This allows users to keep track of movies the have watched or may want
- * to watch. It implements a variety of concepts learned from my CSCI 40 class. 
+ * to watch.There are functions that prints all the movies with their information 
+ * according to sorts. It implements a variety of concepts learned from my CSCI 
+ * 40 class. 
+ * 
+ * When the program starts users are prompted to enter 0-4 for different functions.
+ * Inputting 1 brings you to the library where you can input 1 to sort, 2 to search,
+ * and 0 to exit the library. Within the sort function you can enter 0-3 to sort by
+ * name, genre, year or exit to the menu. Entering 2 in the main menu lets you input
+ * movies to the function where you input the name, genre, and year. Within the remove 
+ * and update section you input a movie name in the library to modify it. There are many
+ * confirmations to make sure the user doesn't input errors. You can enter '0' to exit in
+ * all inputs that explicitly say so.
+ * 
 */
 
 #include <iostream>
